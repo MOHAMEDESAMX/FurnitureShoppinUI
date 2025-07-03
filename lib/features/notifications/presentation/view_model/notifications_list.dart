52ef588744188410878c814d1b6d26d0eb7c7f30
mohamedessam.5530 <mohamedessam.5530@gmail.com>
2025-07-03 13:25:51 +0000

    list name changed

--- lib/features/notifications/presentation/view_model/notifications_list.dart
+++ lib/features/notifications/presentation/view_model/notifications_list.dart
@@ -1,53 +1,53 @@
-class notifications {
+class Notifications {
   final String title;
   final String subtitle;
   final String imageUrl;
 
-  notifications({
+  Notifications({
     required this.title,
     required this.subtitle,
     required this.imageUrl,
   });
 }
 
-List<notifications> mynotifications = [
-  notifications(
+List<Notifications> mynotifications = [
+  Notifications(
       title: "Your order #123456789 has been confirmed",
       subtitle: "Lorem ievem dolar it amet, consectotu: udipiscing eft. Porçis pootluo et in arca adipiscing nec.", imageUrl: 'assets/images/jonny-caspari-wsvCC6UyKjs-unsplash 1.png',
   ),
-  notifications(
+   Notifications(
       title: "Your order #123456789 has been confirmed",
       subtitle: "Lorem ievem dolar it amet, consectotu: udipiscing eft. Porçis pootluo et in arca adipiscing nec.", imageUrl: 'assets/images/sarah-dorweiler-fr0J5-GIVyg-unsplash 1.png',
   ),
-  notifications(
+  Notifications(
       title: "Your order #123456789 has been confirmed",
       subtitle: "Lorem ievem dolar it amet, consectotu: udipiscing eft. Porçis pootluo et in arca adipiscing nec.", imageUrl: 'assets/images/jonny-caspari-wsvCC6UyKjs-unsplash 1.png',
   ),
-  notifications(
+  Notifications(
       title: "Your order #123456789 has been confirmed",
       subtitle: "Lorem ievem dolar it amet, consectotu: udipiscing eft. Porçis pootluo et in arca adipiscing nec.", imageUrl: 'assets/images/jonny-caspari-wsvCC6UyKjs-unsplash 1.png',
   ),
-  notifications(
+  Notifications(
       title: "Your order #123456789 has been confirmed",
       subtitle: "Lorem ievem dolar it amet, consectotu: udipiscing eft. Porçis pootluo et in arca adipiscing nec.", imageUrl: 'assets/images/3968901 1.png',
   ),
-  notifications(
+  Notifications(
       title: "Your order #123456789 has been confirmed",
       subtitle: "Lorem ievem dolar it amet, consectotu: udipiscing eft. Porçis pootluo et in arca adipiscing nec.", imageUrl: 'assets/images/Mask Group.png',
   ),
-  notifications(
+  Notifications(
       title: "Your order #123456789 has been confirmed",
       subtitle: "Lorem ievem dolar it amet, consectotu: udipiscing eft. Porçis pootluo et in arca adipiscing nec.", imageUrl: 'assets/images/jonny-caspari-wsvCC6UyKjs-unsplash 1.png',
   ),
-  notifications(
+  Notifications(
       title: "Your order #123456789 has been confirmed",
       subtitle: "Lorem ievem dolar it amet, consectotu: udipiscing eft. Porçis pootluo et in arca adipiscing nec.", imageUrl: 'assets/images/jonny-caspari-wsvCC6UyKjs-unsplash 1.png',
   ),
-  notifications(
+  Notifications(
       title: "Your order #123456789 has been confirmed",
       subtitle: "Lorem ievem dolar it amet, consectotu: udipiscing eft. Porçis pootluo et in arca adipiscing nec.", imageUrl: 'assets/images/sarah-dorweiler-fr0J5-GIVyg-unsplash 1.png',
   ),
-  notifications(
+  Notifications(
       title: "Your order #123456789 has been confirmed",
       subtitle: "Lorem ievem dolar it amet, consectotu: udipiscing eft. Porçis pootluo et in arca adipiscing nec.", imageUrl: 'assets/images/Mask Group.png',
   ),
